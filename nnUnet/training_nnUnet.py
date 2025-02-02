2ee9a3d261c2b2ba7f080a2b2c92beb30806377f
leoacpr <leo.leroy@ensae.fr>
2025-02-02 20:00:52 +0000

    importing nnU-Net

--- nnUnet/training_nnUnet.py
+++ nnUnet/training_nnUnet.py
@@ -1,3 +1,13 @@
 # Training nnUnet
 
-import numpy
+import torch
+#pip install nnunetv2
+
+
+x = torch.tensor([1.0, 2.0, 3.0])
+print(x)
+x_gpu = torch.randn(3, 3).cuda()
+print(x_gpu)
+
+#nnU-Net needs environment variables
+import nnunetv2
