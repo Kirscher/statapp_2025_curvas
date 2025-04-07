7ce2d39be0d662ba137e938af7ddac416cff326d
concordance <the4pe18@gmail.com>
2025-04-07 14:12:25 +0000

    structure

---
+++ MOVE TEMP/run.sh
@@ -0,0 +1,13 @@
+echo "=== Réglage des variables d'environnement ==="
+
+export nnUNet_raw="/home/onyxia/work/statapp_2025_curvas/nnUNet_raw"
+export nnUNet_preprocessed="/home/onyxia/work/statapp_2025_curvas/nnUNet_preprocessed"
+export nnUNet_results="/home/onyxia/work/statapp_2025_curvas/nnUNet_results"
+
+echo "=== Pré-processing nnUNet (475_CURVAS) ==="
+
+nnUNetv2_plan_and_preprocess -d 475 --verify_dataset_integrity -c 3d_fullres
+
+echo "=== Entraînement NNUnet (475_CURVAS) ==="
+
+nnUNetv2_train 475 3d_fullres 0 --npz
