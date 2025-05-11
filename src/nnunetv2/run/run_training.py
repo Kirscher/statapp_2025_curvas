57088511eacf8bc7bb0dfe6f153ddf09ea1affda
Rémy SIAHAAN--GENSOLLEN <the4pe18@gmail.com>
2025-05-11 02:16:30 +0200

    fix

--- src/nnunetv2/run/run_training.py
+++ src/nnunetv2/run/run_training.py
@@ -15,7 +15,7 @@ from nnunetv2.run.load_pretrained_weights import load_pretrained_weights
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.dataset_name_id_conversion import maybe_convert_to_dataset_name
 from nnunetv2.utilities.find_class_by_name import recursive_find_python_class
-
+import logging
 
 def find_free_network_port() -> int:
     """Finds a free port on localhost.
