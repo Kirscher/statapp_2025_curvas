194e52afa249450efe44b5cadd9be5acc05e9f9a
lab <lucas.cumunel@ensae.fr>
2025-05-16 11:40:51 +0000

    automatisation of metrics

--- Metrics/Metrics_func/new_metrics.py
+++ Metrics/Metrics_func/new_metrics.py
@@ -581,8 +581,8 @@ def sitk_to_array(sitk_img):
     array = sitk.GetArrayFromImage(sitk_img)  # Gives shape (slices, H, W)
     return array.astype(np.float32)
 
-def files_to_data(result_file, prob_file, gt_folder):
-    # obtaining files
+
+def getting_gt(gt_folder): 
     gt_files = sorted([f for f in os.listdir(gt_folder) if "annotation" in f])
     ct_file = next(f for f in os.listdir(gt_folder) if "image" in f)
 
@@ -595,6 +595,11 @@ def files_to_data(result_file, prob_file, gt_folder):
     # CT scan
     ct_img = sitk.ReadImage(os.path.join(gt_folder, ct_file))
     ct_array = sitk_to_array(ct_img)
+    return ct_array, annotations
+
+
+def files_to_data(result_file, prob_file):
+    # obtaining files
 
     # dowloading prediction from nnU-Net
     bin_pred_img = sitk.ReadImage(result_file)
@@ -609,7 +614,7 @@ def files_to_data(result_file, prob_file, gt_folder):
     liver_conf = prob_array[3]
 
     results = [bin_pred, pancreas_conf, kidney_conf, liver_conf]
-    return ct_array, annotations, results
+    return results
 
 '''
 Applying the metrics to the inputed data
@@ -646,16 +651,16 @@ def compute_fast_metrics_parallel(args):
     return entropy_gt, entropy_pred, hausdorff_distances, ace_dict, ncc_dict
 
 
-def apply_metrics(l_patient_files):
+def apply_metrics(l_model_files, ct_image, annotations):
     '''
     Apply selected metrics with simple and clear parallelization.
     '''
     # Extract patient ID
-    ct_name = re.findall(r"\/([^\/]+)$", l_patient_files["gt"])[0]
+    ct_name = re.findall(r"\/([^\/]+)$", l_patient_files["pred"])[0]
 
     # Load and preprocess data
-    ct_image, annotations, results = files_to_data(
-        l_patient_files["pred"], l_patient_files["prob"], l_patient_files["gt"]
+    results = files_to_data(
+        l_model_files["pred"], l_model_files["prob"]
     )
     box_start, box_end = compute_patient_crop_box(annotations)
     cropped_annotations, cropped_bin_pred, cropped_prob_pred = preprocess_results(
@@ -748,82 +753,18 @@ def apply_metrics(l_patient_files):
 """
 Gather the input locations and apply the metrics to all predictions.
 The input folder should be looking like this : 
--- Folder   -- Patient_01   -- pred_01.nii.gz
+-- Folder   -- Model_01   -- pred_01.nii.gz
                             -- pred_prob_01.npz
                             -- GT_01                -- image.nii.gz
                                                     -- annotation_1.nii.gz
                                                     -- annotation_2.nii.gz
                                                     -- annotation_3.nii.gz
-            -- Patient_02   -- pred_02.nii.gz
+            -- Model_02   -- pred_02.nii.gz
             ...
             ...
             ...
+            -- GT   -- image.nii.gz
+                    -- annotation_1.nii.gz
+                    -- annotation_2.nii.gz
+                    -- annotation_3.nii.gz
 """
-
-#Locating data
-data_path = "/home/onyxia/statapp_2025_curvas/truc"#input(str("Path to the folder with all the patient folders : "))
-l_patients_path = os.listdir(data_path)
-l_patients=[]
-
-for patient_dir in l_patients_path:
-    patient_dict = {"pred": None, "prob": None, "gt": None}
-    patient_path = os.path.join(data_path, patient_dir)
-    for item in os.listdir(patient_path):
-            item_path = os.path.join(patient_path, item)
-            if item.endswith(".nii.gz"):
-                patient_dict["pred"] = item_path
-            elif item.endswith(".npz"):
-                patient_dict["prob"] = item_path
-            elif os.path.isdir(item_path):
-                patient_dict["gt"] = item_path
-
-    l_patients.append(patient_dict)
-
-#Prepare output
-df = pd.DataFrame(columns=[
-        "CT",
-        "DICE_panc",
-        "DICE_kidn",
-        "DICE_livr",
-        "CONF_panc",
-        "CONF_kidn",
-        "CONF_livr",
-        "Entropy_GT",
-        "Entropy_Pred",
-        "Hausdorff_1",
-        "Hausdorff_2",
-        "Hausdorff_3",
-        "ECE_1",
-        "ECE_2",
-        "ECE_3",
-        "ACE_1",
-        "ACE_2",
-        "ACE_3",
-        "AUROC_panc",
-        "AUROC_kidn",
-        "AUROC_livr",
-        "AURC_panc",
-        "AURC_kidn",
-        "AURC_livr",
-        "EAURC_panc",
-        "EAURC_kidn",
-        "EAURC_livr",
-        "CRPS_panc",
-        "CRPS_kidn",
-        "CRPS_livr",
-        "NCC_GT1-2",
-        "NCC_GT1-3",
-        "NCC_GT2-3",
-        "NCC_mean"
-    ])
-
-#Computing the metrics
-
-for f in l_patients : 
-    current_line = pd.DataFrame([apply_metrics(f)])
-    df = pd.concat([df,current_line], ignore_index=True)
-
-#Export data
-df.to_csv("metrics.csv", index=False)
-print(df)
-
--- Metrics/run_metrics.py
+++ Metrics/run_metrics.py
@@ -1,44 +1,55 @@
 import os
-from Metrics_func.new_metrics import apply_metrics
+from Metrics_func.new_metrics import apply_metrics, getting_gt
+import boto3
+import pandas as pd
+
+# inputs
+aws_access_key_id = "MPOO6QLTXGQVW61QK4ZE"#input(str("aws_access_key_id : "))
+aws_secret_access_key = "LeZYGLEp99y+IkkKBRkRaNVdkc2YHK+lekNx+L+f"#input(str("aws_access_key_key : "))
+aws_session_token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJNUE9PNlFMVFhHUVZXNjFRSzRaRSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sImF1ZCI6WyJtaW5pby1kYXRhbm9kZSIsIm9ueXhpYSIsImFjY291bnQiXSwiYXV0aF90aW1lIjoxNzQ2OTEwOTAxLCJhenAiOiJvbnl4aWEiLCJlbWFpbCI6Imx1Y2FzLmN1bXVuZWxAZW5zYWUuZnIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZXhwIjoxNzQ3NTE1ODk5LCJmYW1pbHlfbmFtZSI6IkN1bXVuZWwiLCJnaXZlbl9uYW1lIjoiTHVjYXMiLCJncm91cHMiOlsiVVNFUl9PTllYSUEiLCJzdGF0YXBwLXNlZ21lZGljIl0sImlhdCI6MTc0NjkxMTA5OSwiaXNzIjoiaHR0cHM6Ly9hdXRoLmxhYi5zc3BjbG91ZC5mci9hdXRoL3JlYWxtcy9zc3BjbG91ZCIsImp0aSI6Im9ucnRydDo2NWE4Zjc1ZS03NjZlLTRhZmEtYTJjZC03ZjQwMzY0MjI4OWYiLCJuYW1lIjoiTHVjYXMgQ3VtdW5lbCIsInBvbGljeSI6InN0c29ubHkiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJsYWIiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtc3NwY2xvdWQiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1zc3BjbG91ZCJdLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGdyb3VwcyBlbWFpbCIsInNpZCI6IjA3ODg3YzIwLTFjMmUtNDliMC1iMTg5LWE4OGU0OGYxM2NjNCIsInN1YiI6ImUyZDc4NjRjLTcwMzItNDI0ZC04OTA2LWU0ZjhiNDFjYzAwMyIsInR5cCI6IkJlYXJlciJ9.ga6x727DgquSCMmy-I609uoqDHAw0zGcPBN2ayD8PmE_Gp9sSerqAgJ5Yfqxl1XmGq6WWzqPeDvnxBYIBpgWyw"#input(str("aws_session_token : "))
+patient = "UKCHLL007"#input(str("Patient ID (name of the folder with all the models) :"))
+pred_patient_path = "projet-statapp-segmedic/output/UKCHLL007"#input(str("Path to folder with predictions in s3 (only bucket/.../folder) :"))
+gt_patient_path = "projet-statapp-segmedic/data/UKCHLL007"#input(str("Path to folder with GT in s3 (only bucket/.../folder) :"))
+#output_path = input(str("Path to storage for metrics output (only bucket/.../folder) :"))
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url='https://minio.lab.sspcloud.fr',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    aws_session_token=aws_session_token
+)
 
 
+pred_command = f"aws s3 cp s3://{pred_patient_path} ./{patient} --endpoint-url https://minio.lab.sspcloud.fr --recursive"
+gt_command = f"aws s3 cp s3://{gt_patient_path} ./{patient}/{patient}_GT --endpoint-url https://minio.lab.sspcloud.fr --recursive"
+output_command = f"aws s3 cp ./metrics.csv s3://metrics_results/{patient}/metrics.csv --endpoint-url https://minio.lab.sspcloud.fr"
 
-#BODY
-"""
-Gather the input locations and apply the metrics to all predictions.
-The input folder should be looking like this : 
--- Folder   -- Patient_01   -- pred_01.nii.gz
-                            -- pred_prob_01.npz
-                            -- GT_01                -- image.nii.gz
-                                                    -- annotation_1.nii.gz
-                                                    -- annotation_2.nii.gz
-                                                    -- annotation_3.nii.gz
-            -- Patient_02   -- pred_02.nii.gz
-            ...
-            ...
-            ...
-"""
+# s3.upload_file("metrics.csv", "projet-statapp-segmedic", "metrics_results/patient/metrics.csv")
 
-#Locating data
-data_path = "/home/onyxia/statapp_2025_curvas/truc"#input(str("Path to the folder with all the patient folders : "))
-l_patients_path = os.listdir(data_path)
-l_patients=[]
+os.system(pred_command)
+os.system(gt_command)
 
-for patient_dir in l_patients_path:
-    patient_dict = {"pred": None, "prob": None, "gt": None}
-    patient_path = os.path.join(data_path, patient_dir)
-    for item in os.listdir(patient_path):
-            item_path = os.path.join(patient_path, item)
-            if item.endswith(".nii.gz"):
-                patient_dict["pred"] = item_path
-            elif item.endswith(".npz"):
-                patient_dict["prob"] = item_path
-            elif os.path.isdir(item_path):
-                patient_dict["gt"] = item_path
 
-    l_patients.append(patient_dict)
+# Locating data
+data_path = f"./{patient}"
+l_models_path = os.listdir(data_path)
+l_models_path = [item for item in l_models_path if "GT" not in item]
+l_models = []
 
-#Prepare output
+for model_dir in l_models_path:
+    model_dict = {"pred": None, "prob": None, "name": re.findall(r"\/([^\/]+)$",model_dir)}
+    model_path = os.path.join(data_path, model_dir)
+    for item in os.listdir(model_path):
+        item_path = os.path.join(model_path, item)
+        if item.endswith(".nii.gz"):
+            model_dict["pred"] = item_path
+        elif item.endswith(".npz"):
+            model_dict["prob"] = item_path
+    l_models.append(model_dict)
+gt_path = os.path.join(data_path, os.path.basename(str(data_path+"_GT")))
+
+# Prepare output
 df = pd.DataFrame(columns=[
         "CT",
         "DICE_panc",
@@ -76,12 +87,13 @@ df = pd.DataFrame(columns=[
         "NCC_mean"
     ])
 
-#Computing the metrics
+# Computing the metrics
+#TODO, prio ensemble
+for f in l_models: 
+    current_line = pd.DataFrame([apply_metrics(f, getting_gt(gt_path))])
+    df = pd.concat([df, current_line], ignore_index=True)
 
-for f in l_patients : 
-    current_line = pd.DataFrame([apply_metrics(f)])
-    df = pd.concat([df,current_line], ignore_index=True)
-
-#Export data
+# Export data
 df.to_csv("metrics.csv", index=False)
 print(df)
+#os.system(output_command)
